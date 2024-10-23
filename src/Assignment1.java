fb77387e93cbfdab3533208202e6e370e3da80d1
sdk194 <seandags750@gmail.com>
2024-10-23 17:34:52 +0100

    Refactor DH gen to be more generic

--- src/Assignment1.java
+++ src/Assignment1.java
@@ -29,6 +29,17 @@ public class Assignment1 {
     static String generatorHex = "44ec9d52c8f9189e49cd7c70253c2eb3154dd4f08467a64a0267c9defe4119f2e373388cfa350a4e66e432d638ccdc58eb703e31d4c84e50398f9f91677e88641a2d2f6157e2f4ec538088dcf5940b053c622e53bab0b4e84b1465f5738f549664bd7430961d3e5a2e7bceb62418db747386a58ff267a9939833beefb7a6fd68";
     static String givenKeyHex = "5af3e806e0fa466dc75de60186760516792b70fdcd72a5b6238e6f6b76ece1f1b38ba4e210f61a2b84ef1b5dc4151e799485b2171fcf318f86d42616b8fd8111d59552e4b5f228ee838d535b4b987f1eaf3e5de3ea0c403a6c38002b49eade15171cb861b367732460e3a9842b532761c16218c4fea51be8ea0248385f6bac0d";
 
+    private static void saveDH(String dh) {
+        try {
+            FileWriter dhFile = new FileWriter("DH.txt");
+            dhFile.write(dh);
+            dhFile.close();
+        } catch (IOException e) {
+            System.out.println("Error");
+            e.printStackTrace();
+        }
+    }
+
     public static BigInteger myModPow(BigInteger n, BigInteger pwr, BigInteger mod) {
         BigInteger curr = new BigInteger("1");
         String binary = pwr.toString(2);
@@ -36,35 +47,31 @@ public class Assignment1 {
             curr = curr.multiply(curr);
             if (binary.charAt(i) == '1') {
                 curr = curr.multiply(n);
-            };
+            }
             curr = curr.mod(mod);
         }
         return curr;
     }
 
     // Generate value for DH.txt
-    public static void genDH(BigInteger g, String secret, BigInteger p) {
+    public static BigInteger genValues(BigInteger n, String secret, BigInteger p, Boolean isDH) {
         BigInteger b = new BigInteger(secret);
-        BigInteger dh = myModPow(g, b, p);
+        BigInteger value = myModPow(n, b, p);
 
-        try {
-            FileWriter dhFile = new FileWriter("DH.txt");
-            dhFile.write(dh.toString(16));
-            dhFile.close();
+        if (isDH) {
+            saveDH(value.toString(16));
         }
-        catch (IOException e) {
-            System.out.println("Error");
-            e.printStackTrace();
-        }
+        return value;
     }
 
-    // Secret key, remove once diffle-hellmen agreement is finished implemented: 70390995917764513604254768192752813927828121340161866492659666896520349237363420686605146457662486750682910739161370950599554944903809582867798075682681947405884792026785104262651707960691174598872062238218916187716833008177236858888577427594708180069071351782554589687319103134362024870880193867005087176969
     public static void main(String[] args) {
         BigInteger prime = new BigInteger(primeHex, 16);
         BigInteger generator = new BigInteger(generatorHex, 16);
         BigInteger givenKey = new BigInteger(givenKeyHex, 16);
 
-        genDH(generator, args[0], prime);
+        // Uncomment to generate a new DH, takes first argument from commandline as secret key
+        // Secret key used: 70390995917764513604254768192752813927828121340161866492659666896520349237363420686605146457662486750682910739161370950599554944903809582867798075682681947405884792026785104262651707960691174598872062238218916187716833008177236858888577427594708180069071351782554589687319103134362024870880193867005087176969
+        // genValues(generator, args[0], prime, true);
 
 
         BigInteger base = new BigInteger("2");
